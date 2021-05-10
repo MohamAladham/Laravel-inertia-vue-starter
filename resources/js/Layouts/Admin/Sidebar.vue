430ced84dbbe6557344d4fcaed42dbd2c92d579b
Mohammed <Mohammedaladham101@gmail.com>
2021-05-10 05:09:37 +0300

    Fix sidebar link tag

--- resources/js/Layouts/Admin/Sidebar.vue
+++ resources/js/Layouts/Admin/Sidebar.vue
@@ -4,12 +4,13 @@
         <div class="navbar-header">
             <ul class="nav navbar-nav flex-row">
                 <li class="nav-item mr-auto">
-                    <a class="navbar-brand" :href="route('admin.dashboard')">
+                    <inertia-link class="navbar-brand" :href="route('admin.dashboard')">
                         <span class="brand-logo">
                              <img src="/assets/admin/images/logo/logo.png">
                         </span>
                         <h2 class="brand-text">{{ this.$page.props.appName }}</h2>
-                    </a></li>
+                    </inertia-link>
+                </li>
                 <li class="nav-item nav-toggle">
                     <a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="d-block d-xl-none text-primary toggle-icon font-medium-4" data-feather="x"></i><i class="d-none d-xl-block collapse-toggle-icon font-medium-4  text-primary" data-feather="disc" data-ticon="disc"></i></a>
                 </li>
@@ -20,40 +21,34 @@
             <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
 
                 <li v-for="link in links" :key="link.title" :class="{'active':link.isActive}" class="nav-item">
-                    <a class="d-flex align-items-center" :href="link.url">
-                        <i :data-feather="link.icon"></i>
-                        <span class="menu-title text-truncate">{{ link.title }}</span>
-                        <span v-if="link.badge" class="badge badge-light-danger badge-pill ml-auto mr-1">{{ link.badge }}</span>
-                    </a>
 
-                    <ul v-if="link.subLinks" class="menu-content">
-                        <li v-for="subLink in link.subLinks" :key="link.title + subLink.title" :class="{'active':subLink.isActive}">
-                            <a class="d-flex align-items-center" :href="subLink.url">
-                                <i data-feather="circle"></i>
-                                <span class="menu-item">{{ subLink.title }}</span>
-                            </a>
-                        </li>
-                    </ul>
+                    <template v-if="!link.subLinks">
+                        <inertia-link class="d-flex align-items-center" :href="link.url">
+                            <i :data-feather="link.icon"></i>
+                            <span class="menu-title text-truncate">{{ link.title }}</span>
+                            <span v-if="link.badge" class="badge badge-light-danger badge-pill ml-auto mr-1">{{ link.badge }}</span>
+                        </inertia-link>
+                    </template>
+
+                    <template v-else>
+                        <a class="d-flex align-items-center" :href="link.url">
+                            <i :data-feather="link.icon"></i>
+                            <span class="menu-title text-truncate">{{ link.title }}</span>
+                            <span v-if="link.badge" class="badge badge-light-danger badge-pill ml-auto mr-1">{{ link.badge }}</span>
+                        </a>
+
+                        <ul v-if="link.subLinks" class="menu-content">
+                            <li v-for="subLink in link.subLinks" :key="link.title + subLink.title" :class="{'active':subLink.isActive}">
+                                <inertia-link class="d-flex align-items-center" :href="subLink.url">
+                                    <i data-feather="circle"></i>
+                                    <span class="menu-item">{{ subLink.title }}</span>
+                                </inertia-link>
+                            </li>
+                        </ul>
+                    </template>
 
                 </li>
 
-                <!--
-                <li class=" nav-item">
-                    <a class="d-flex align-items-center" href="#">
-                    <i data-feather="layout"></i>
-                    <span class="menu-title text-truncate">Page Layouts</span>
-                    <span class="badge badge-light-danger badge-pill ml-auto mr-1">2</span>
-                    </a>
-                    <ul class="menu-content">
-                        <li>
-                            <a class="d-flex align-items-center" href="layout-blank.html">
-                            <i data-feather="circle"></i>
-                            <span class="menu-item">Layout Blank</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                                -->
 
             </ul>
         </div>
